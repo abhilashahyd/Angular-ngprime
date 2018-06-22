1569eee054843908655a382e873c3e8f14a9d787
Abhilasha Sinha <abhilashahyd@gmail.com>
2018-06-22 12:31:46 +0530

    Sidemenu added

---
+++ ReportingApp/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {SidebarModule} from 'primeng/sidebar';
+import { PanelMenuModule } from 'primeng/panelmenu';
+// import { MenuItem }  from 'primeng/api';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    SidemenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    SidebarModule,
+    PanelMenuModule
+    // MenuItem
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ReportingApp/src/app/sidemenu/sidemenu.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+// import { MenuModule } from 'primeng/menu';
+import { MenuItem }  from 'primeng/api';
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styleUrls: ['./sidemenu.component.css']
+})
+export class SidemenuComponent implements OnInit {
+ private items : MenuItem[];
+ display = true;
+
+
+  constructor() { }
+
+  ngOnInit() {
+  this.items =[{label:'Employee'},{label:'Utilization'}];
+  }
+
+}
